fc00c99ed3293eb4d0c79bb07e58182923a45eb7
64309010023 <64309010023@gmail.com>
2021-12-02 16:21:20 +0700

    demo

--- showdemo.php
+++ showdemo.php
@@ -17,7 +17,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
   // output data of each row
   while($row = mysqli_fetch_assoc($result)) {
-    echo "Id: " . $row["Id"]. " - Name: " . $row["name"]. " -address: " . $row["address"]. "<br>";
+    echo "Id: " . $row["Id"]. " - name: " . $row["name"]. " -address: " . $row["address"]. "<br>";
   }
 } else {
   echo "0 results";
